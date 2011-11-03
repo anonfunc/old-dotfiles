43db70b4813a94eaecabf492356e82c1db95783d
anonfunc <anonfunc@gmail.com>
2011-11-03 16:31:02 -0700

    Moving functions either out of ~/bin or 75-functions.zsh into the auto-load directory.

--- home/.zsh.d/75-functions.zsh
+++ home/.zsh.d/75-functions.zsh
@@ -1,52 +1,9 @@
 # depends on aliases.
 
-# mwittenberg's wdservices function
-wdservices () {
-	    ps -eaf | grep '[j]ava.*-Dwd.tag' \
-		    | sed -e 's/\([0-9][0-9]*\) *\([0-9][0-9]*\) *.*\(tag=[^ ]*\) .*/\1 \2 \3/'
-}
-# show wd.sys too.
-function wd {
-        if [ -z "$*" ] ; then
-                wd tag sys      
-        else
-                OLDIFS="$IFS"
-                IFS="
-"
-                for LINE in `ps -eaf | grep '[j]ava.*-Dcatalina.home'`
-                do
-                        echo -n "$LINE" | awk '{ printf("%s\t%s\t", $1,$2); }'
-                        for ARG in $*
-                        do
-                                echo -n "$LINE" | grep "$ARG" |sed -e "s/^.*\\($ARG=\\?[^ ]*\\).*$/\\1/" \
-                                                | tr -d '\n' 
-                                echo -n -e "\t"
-                        done
-                        echo
-                done
-                IFS="$OLDIFS"
-        fi
-}
-
-wdnice () {
-    OLDIFS="$IFS"
-	IFS="
-"
-	for LINE in `ps -eaf | grep '[j]ava.*-Dcatalina.home'`
-	do
-	    	echo "----------------------"
-#		IFS=" "
-		echo "$LINE" | sed -e "s/ -\\([DXa-z]\\)/\n -\\1/g"
-		echo "----------------------"
-		echo 
-	done
-    IFS="$OLDIFS"
-}
-
 # excel completion
 
 _excel () {
-	compadd `ls *.xml *.xls *.xlsx`
+       	compadd `ls *.xml *.xls *.xlsx`
 }
 compdef _excel excel
 
@@ -90,12 +47,5 @@ function d()
     #fi
 #}
 #compdef _directories cd_to_dir
-#alias cd=cd_to_dir 
-
-
-function lo() {
-    PAT=$1
-    shift
-    locate $* "`pwd`/$PAT" "`pwd`/*/$PAT" 
-}
+#alias cd=cd_to_dir
 
