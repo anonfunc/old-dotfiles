04ccf4a9ec5dc0ebb80ed87d971210340cec7691
anonfunc <anonfunc@gmail.com>
2012-01-18 11:12:00 -0800

    SH: new android paths

--- home/.zsh.d/01-vars.zsh
+++ home/.zsh.d/01-vars.zsh
@@ -7,7 +7,7 @@ export PATH="$HOME/bin/git-scripts:$PATH"
 #export PATH="$HOME/bin/apps/eclipse:$PATH"
 export PATH="$HOME/bin/apps/jira-cli-1.5.0:$PATH"
 export PATH="$HOME/bin/apps/scala/bin:$PATH"
-export PATH="$HOME/Playground/android-sdk-linux_x86/tools:$PATH"
+export PATH="$HOME/Playground/android-sdk-linux/tools:$PATH"
 export PATH="/var/lib/gems/1.8/bin:$PATH"
 export PATH="$HOME/.cabal/bin:$PATH"
 export PATH="$PATH:/opt/openoffice.org3/program"
@@ -22,7 +22,7 @@ export ALTERNATE_EDITOR="/usr/local/bin/vim"
 export VISUAL="emacsclient -c -t"
 export GDK_NATIVE_WINDOWS=1
 export MAILDIR="/home/first/Mail"
-export ANDROID_HOME="/home/first/Playground/android-sdk-linux_x86"
+export ANDROID_HOME="/home/first/Playground/android-sdk-linux"
 
 
 export REALTERM=${REALTERM:-$TERM}
